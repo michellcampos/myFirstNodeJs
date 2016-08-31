25b9e400a088c0a34c9009153a5aaf04dd3a9536
Michell Campos <michellcampos13@gmail.com>
2016-08-31 11:05:51 -0300

    added taxvat validation

--- public/javascripts/validate.js
+++ public/javascripts/validate.js
@@ -48,6 +48,12 @@
                     return false;
                 }
                 return true;
+            },
+            validTaxvat: function(input) {
+                if(input.is("[name=taxvat]")) {
+                    return taxvatValidate(input.val());
+                }
+                return true;
             }
         },
         messages: {
@@ -68,7 +74,8 @@
                 else {
                     return msg(input.val());
                 }
-            }
+            },
+            validTaxvat: 'Invalid taxvat'
         }
     }).data('kendoValidator');
     var availability = {
@@ -92,5 +99,25 @@
                 }
             });
         }
+    };
+    var taxvatValidate = function (strCPF) {
+        var Soma = 0, Resto, i;
+        var strCPF = strCPF.replace(/[\.-]/g, "");
+
+        if (strCPF == "00000000000") return false;
+
+        for (i=1; i<=9; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(strCPF.substring(9, 10)) ) return false;
+
+        Soma = 0;
+        for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i-1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(strCPF.substring(10, 11) ) ) return false;
+        return true;
     };
 }());
